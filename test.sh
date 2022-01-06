e000d90b8cfa161be2c652d42207892735963e3d
farazs1e1 <47537932+farazs1e1@users.noreply.github.com>
2022-01-06 14:43:00 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export password=$(aws ssm get-parameters --region us-east-1 --names patch_id --with-decryption --query Parameters[0].Value)
-sudo chmod 777  /var/www/html/index.html
+sudo chmod -R 777 /var/www/html/index.html
 echo $password >> /var/www/html/index.html
 
