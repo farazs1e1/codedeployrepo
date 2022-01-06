1c392059c602e8eb43e742648dba83ac51633fcc
farazs1e1 <47537932+farazs1e1@users.noreply.github.com>
2022-01-06 14:20:31 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 password=$(aws ssm get-parameters --region us-east-1 --names patch_id --with-decryption --query Parameters[0].Value)
-echo $password 
+echo $password > /var/www/html/index.html
